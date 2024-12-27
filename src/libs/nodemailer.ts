f42fef73168dad5e5465e105a2030e4db9770bda
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-12-27 11:34:16 +0600

    util to setup nodemailer trnasporter

---
+++ src/libs/nodemailer.ts
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+export const createTransporter = async () => {
+  const { token } = await oauth2Client.getAccessToken();
+  // Create a transporter
+  const trnasporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.SENDER_EMAIL_ADDRESS,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: token || '',
+    },
+    from: process.env.SENDER_EMAIL_ADDRESS,
+  });
+
+  // Verify transporter
+  trnasporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+      throw new Error(error.message);
+    } else {
+      console.log('Server is ready to take our messages', success);
+    }
+  });
+
+  return trnasporter;
+};
